5a2bf0e1ea0cce9b15a2bee5d0383a40daf4de83
Edward Samuel Pasaribu <edwardsamuel92@gmail.com>
2017-12-14 19:35:29 +0800

    Update mac-setup.sh

--- mac-setup.sh
+++ mac-setup.sh
@@ -3,7 +3,7 @@
 # Install XCode tools
 xcode-select --install
 
-# Install Homebrew 
+# Install Homebrew
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew update && brew outdated
 brew upgrade && brew cleanup
@@ -15,9 +15,8 @@ cat brew_cask_list | xargs brew cask install
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 # Install dotfiles
-ls | grep dot- | cut -d '-' -f 2 | xargs -n 1 -I {} ln -s -f `pwd`/dot-{} ~/.{}
-
-gcloud components install kubectl
+ls -p | grep -v / | grep dot- | cut -d '-' -f 2 | xargs -n 1 -I {} ln -s -f `pwd`/dot-{} ~/.{}
+ls -p | grep / | grep dot- | cut -d '-' -f 2 | xargs -n 1 -I {} sh -c '[ ! -e ~/.{} ] && ln -s -f `pwd`/dot-{} ~/.{}'
 
 git submodule init
 git submodule update
