92a04e67d524624a7a303358706d35836614ce86
Edward Samuel Pasaribu <edwardsamuel92@gmail.com>
2019-06-05 00:36:17 +0800

    Fix linking dot directory; Install i2cssh;

--- mac-setup.sh
+++ mac-setup.sh
@@ -5,11 +5,11 @@ xcode-select --install
 
 # Install Homebrew
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-cat brew_list | xargs brew install
-cat brew_tap | xargs brew tap
+cat brew_list | grep -v ^# | xargs brew install
+cat brew_tap | grep -v ^# | xargs brew tap
 brew update && brew outdated
 brew upgrade && brew cleanup
-cat brew_cask_list | xargs brew cask install
+cat brew_cask_list | grep -v ^# | xargs brew cask install
 brew cask outdated --quiet | xargs brew cask reinstall
 
 # Setup Oh-My-ZSH
@@ -17,7 +17,7 @@ sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/i
 
 # Install dotfiles
 ls -p | grep -v / | grep dot- | cut -d '-' -f 2 | xargs -n 1 -I {} ln -s -f `pwd`/dot-{} ~/.{}
-ls -p | grep / | grep dot- | cut -d '-' -f 2 | xargs -n 1 -I {} sh -c '[ ! -e ~/.{} ] && ln -s -f `pwd`/dot-{} ~/.{}'
+ls -p | grep / | grep dot- | cut -d '-' -f 2 | cut -d '/' -f 1 | xargs -n 1 -I {} sh -c '[ ! -e ~/.{} ] && ln -s -f `pwd`/dot-{} ~/.{}'
 
 git submodule init
 git submodule update
@@ -30,3 +30,5 @@ git config --global core.excludesfile ~/.gitignore_global
 # Install fzf
 yes | /usr/local/opt/fzf/install
 
+# Install i2cssh
+gem install i2cssh
